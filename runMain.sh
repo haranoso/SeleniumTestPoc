1ff3ce7e63a7f5b52cc9d906daff3a429b528f31
git account name <git account email>
2021-02-18 14:42:36 +0900

    update

--- runMain.sh
+++ runMain.sh
@@ -1,33 +1,31 @@
 #!/bin/bash
 set -eu
 
-echo $SFDX_AUTH_URL
+
+export TESTRESULT="./TestResult/"
+export CHECKRESULT="./checkResult"
+export DIFF="./DiffResult/"
+
+export OLD=$TESTRESULT"old"
+export LATEST=$TESTRESULT"latest"
+
+export DATE=`date "+%Y%m%d_%H%M%S"`
 
 
-# export TESTRESULT="./TestResult/"
-# export CHECKRESULT="./checkResult"
-# export DIFF="./DiffResult/"
+# SETUP SFDX
+echo SETUP SFDX
+echo $SFDX_AUTH_URL > ./key.auth
+sfdx config:set defaultusername=`sfdx for/ce:auth:sfdxurl:store -f ./key.auth |cut -d ' ' -f3`
 
-# export OLD=$TESTRESULT"old"
-# export LATEST=$TESTRESULT"latest"
+# SETUP GIT
+echo SETUP GIT
+git config --global user.email $GIT_EMAIL
+git config --global user.name $GIT_USERNAME
 
-# export DATE=`date "+%Y%m%d_%H%M%S"`
+# START TEST
+ ./runTest.sh
+ ./runGitandPostSlack.sh 
 
-
-# # SETUP SFDX
-# echo SETUP SFDX
-# echo $SFDX_AUTH_URL > ./key.auth
-# sfdx config:set defaultusername=`sfdx for/ce:auth:sfdxurl:store -f ./key.auth |cut -d ' ' -f3`
-
-# # SETUP GIT
-# echo SETUP GIT
-# git config --global user.email $GIT_EMAIL
-# git config --global user.name $GIT_USERNAME
-
-# # START TEST
-#  ./runTest.sh
-#  ./runGitandPostSlack.sh 
-
-# # START CHECK
-# ./runCheck.sh 
+# START CHECK
+./runCheck.sh 
 
